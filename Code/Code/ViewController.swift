9cbbe9db5382bf9334c8d52b10e0e8365134a4b4
Yogesh_Gupta <yogesh2.gupta@ril.com>
2022-02-13 10:48:47 +0530

    Initial Setup

---
+++ Code/Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Code
+//
+//  Created by Yogesh2 Gupta on 13/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
